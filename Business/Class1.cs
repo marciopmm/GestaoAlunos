a945751a8e68f8ecf47751167cc86694cc5232e8
Marcio Martins <marciopmm@gmail.com>
2016-05-08 22:46:06 -0300

    Carga inicial

---
+++ Business/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoAlunos
+{
+    public class Class1
+    {
+    }
+}
